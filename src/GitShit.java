e0bcb5e44368cde2af72f1ee3b3c4604a561fa50
Adam Katry <adamkatry@gmail.com>
2019-09-16 14:50:10 +0200

    d1

--- src/GitShit.java
+++ src/GitShit.java
@@ -5,12 +5,19 @@ public class GitShit {
   public static void main(String[] args) {
 
     cheer();
+    future();
 
   }
 
   private static void cheer() {
 
     System.out.println("KrAdBaGa");
+
+  }
+
+  private static void future() {
+
+    System.out.println("halak");
 
   }
 
