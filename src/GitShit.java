c83500211192741366a2a6fa1c8dd4c04076a794
KrisztinaFoldi <krisztifoldi11@gmail.com>
2019-09-16 14:56:48 +0200

    hello

--- src/GitShit.java
+++ src/GitShit.java
@@ -1,5 +1,11 @@
 public class GitShit {
+  public static void main(String[] args) {
+    System.out.println("Hi my age is: " + count());
+  }
 
+  public static int count() {
+    return 23;
+  }
   //hi all
 
 }
